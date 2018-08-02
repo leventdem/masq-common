04816f4234aca8088abb7e6cdf556877dfe528f9
Levent DEMIR <levent.demir01@gmail.com>
2018-08-02 11:37:47 +0200

    Update errors.js
    Add import rules error

--- src/errors.js
+++ src/errors.js
@@ -17,7 +17,8 @@ const ERRORS = {
   BADREQUEST: 'Bad request',
   NOTAUTHORIZED: 'Not authorized',
   ENCRYPTIONERROR: 'Encryption error',
-  DECRYPTIONERROR: 'Decryption error'
+  DECRYPTIONERROR: 'Decryption error',
+  NOIMPORTRULES: 'No import rules provided'
 }
 
 /**
